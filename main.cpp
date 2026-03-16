bb7d4a9275c5a4a32dd5c9f8053281a9651c97e2
R4mFaaaw <rafamirzafirdaus@gmail.com>
2026-03-16 20:49:47 +0700

    Awal

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream> //cout,cin
+#include <vector> //array,pointer
+#include <string> //string
+#include <cstdlib> //linkedList
+#include <iomanip> //Rapih
+using namespace std; //biar ga usah std::
+
+struct itu_penyakit{
+
+};
+
+void bukannya_meninggal__koid(){
+
+}
+
+int main(){
+    cout << "mau main apa y";
+}
